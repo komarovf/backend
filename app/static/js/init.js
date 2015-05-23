7aee1de69b8a37b45dc6e9a1c4522f4b3709769a
komarov Serhii <komarovf88@gmail.com>
2015-05-23 16:10:54 +0300

    js

--- app/static/js/init.js
+++ app/static/js/init.js
@@ -1,29 +1,6 @@
-head.ready(function() {
-// $(document).on("click", function(){
-// $(".js-popup").hide();
-// });
-// function scrollFixedElements() {
-// var scroll_left = $(this).scrollLeft();
-// $(".fixed-element").css({
-// left: -scroll_left
-// });
-// }
-// scrollFixedElements();
-// $(window).scroll(function(){
-// scrollFixedElements()
-// });
-(document).ready(function() {
-$('.menu').addClass('open');
-('.bt-nav').click( function() {
-$(this).parent().toggleClass( function() {
-if ( $(this).hasClass('open') ) {
-return 'close';
-} else
-if ( $(this).hasClass('close') ) {
-return 'open';
-}
-});
-});
-});
-console.log($('body').html());
-});
+head.load("js/lib/jquery.js",
+					"js/lib/cycle.js",
+					"js/lib/cycle.carousel.js",
+					"js/common.js",
+					"js/jquery.validate.js",
+					"js/picturefill.js");
